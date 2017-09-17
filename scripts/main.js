871af51b363c192eabbebf5b1cb38c44968c2af8
Michael Romero <romerom@gmail.com>
2017-09-16 18:31:21 -0700

    asdlkfj

--- scripts/main.js
+++ scripts/main.js
@@ -57,7 +57,6 @@ function initializeEvents() {
 function randomTacoCat(thumbnails) {
   'use strict';
   var randomSelection = Math.floor(Math.random() * thumbnails.length) + 1;
-  console.log('randomSelection = ' + randomSelection);
   TACOCAT_THUMBNAIL_BACKUP = thumbnails[randomSelection].getAttribute('data-image-url');
   thumbnails[randomSelection].setAttribute('data-image-url', 'https://i.imgur.com/BAJirn6.png');
 }
